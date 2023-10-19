15231b1232196c8e81ca764968a3505454989998
Youmppy <headsch00l@gmail.com>
2023-10-19 14:48:49 -0700

    1

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_SIZE 1024
+
+void run_shell();
+
+#endif
+
